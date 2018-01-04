5f93f4080e1318d02bcdbe2f0edc8d6c4afd3b93
wells86 <calebrwells@gmail.com>
2018-01-04 11:05:13 -0800

    ..

--- 2018/Swift 4 Back to Basics/Simple Values.playground/Contents.swift
+++ 2018/Swift 4 Back to Basics/Simple Values.playground/Contents.swift
@@ -1,5 +1,14 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
+let name = "Caleb"
+var age = 27
 
+
+let myNameAndAge = "My name is \(name), and my age is \(age)"
+
+let multipleLines = """
+    line one
+    line two
+    """
+
+print(multipleLines)
