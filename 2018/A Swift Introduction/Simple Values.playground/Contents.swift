24c2d1e004d630addbd01f3f49ff9ef5f1819f0b
wells86 <calebrwells@gmail.com>
2018-01-08 12:59:37 -0800

    Simple Values

---
+++ 2018/A Swift Introduction/Simple Values.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var valueA: Int
+
+valueA = 11
+
+let valueB = "Hello"
+
+let multipleLines = "line 1 \nline 2"
+
+let hello = "\(valueB) my name is \(valueA)"
+
+var shoppingList = ["dog food", "water", "beer"]
+shoppingList.append("cat food")
+print(shoppingList.count)
+
+
+var dictionary = [String: String]()
