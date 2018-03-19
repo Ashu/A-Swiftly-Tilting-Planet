6849f255e8801e960ea623ace387537b572adf5c
Caleb Wells <calebrwells@gmail.com>
2018-03-19 10:43:09 -0700

    Handling Swipe Gestures.

---
+++ 2018/UIGestureRecognizers/Handling Swipe Gestures/Handling Swipe Gestures/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Handling Swipe Gestures
+//
+//  Created by Caleb Wells on 3/19/18.
+//  Copyright © 2018 Caleb Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(sender:)))
+        // the default direction is right
+        
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(sender:)))
+        leftSwipe.direction = .left
+        
+        let upSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(sender:)))
+        upSwipe.direction = .up
+        
+        let downSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(sender:)))
+        downSwipe.direction = .down
+        
+        view.addGestureRecognizer(rightSwipe)
+        view.addGestureRecognizer(leftSwipe)
+        view.addGestureRecognizer(upSwipe)
+        view.addGestureRecognizer(downSwipe)
+
+    }
+    
+    @objc func handleSwipe(sender: UISwipeGestureRecognizer) {
+        
+        if sender.state == .ended {
+            switch sender.direction {
+            case .right:
+                view.backgroundColor = .red
+            case .left:
+                view.backgroundColor = .blue
+            case .up:
+                view.backgroundColor = .green
+            case .down:
+                view.backgroundColor = .yellow
+            default:
+                break
+            }
+        }
+    }
+}
+
