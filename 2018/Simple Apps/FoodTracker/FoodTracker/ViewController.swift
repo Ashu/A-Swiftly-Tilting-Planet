d24f5744374cf4da6b0880d96766c397bed807fb
Caleb Wells <calebrwells@gmail.com>
2018-04-16 20:19:57 -0700

    FoodTracker

---
+++ 2018/Simple Apps/FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Caleb Wells on 4/16/18.
+//  Copyright © 2018 Caleb Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
