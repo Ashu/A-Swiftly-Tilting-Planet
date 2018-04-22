c5a6c7e928eb86423948c85c8116ae275323596e
Caleb Wells <calebrwells@gmail.com>
2018-04-21 22:35:29 -0700

    still playing with animations.

--- 2018/Swift Animations/Animations/Animations/ViewController.swift
+++ 2018/Swift Animations/Animations/Animations/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let label: UILabel = {
+    let labelOne: UILabel = {
         let label = UILabel()
-        label.text = "Hello, world!"
+        label.text = "Welcome to"
+        label.font = UIFont.boldSystemFont(ofSize: 27)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let labelTwo: UILabel = {
+        let label = UILabel()
+        label.text = "App Name"
         label.font = UIFont.boldSystemFont(ofSize: 27)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
@@ -21,27 +29,45 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(label)
+        view.addSubview(labelOne)
+        view.addSubview(labelTwo)
         
         setUpLayout()
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        label.alpha = 0
+        labelOne.alpha = 0
+        labelTwo.alpha = 0
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        UIView.animate(withDuration: 2.5) {
-            self.label.alpha = 1
-            self.label.frame.origin.y -= 100
+//        UIView.animate(withDuration: 2.5) {
+//            self.label.alpha = 1
+//            self.label.frame.origin.y -= 100
+//        }
+        
+        UIView.animate(withDuration: 2.0, delay: 0, options: [.curveEaseOut], animations: {
+            self.labelOne.alpha = 1
+            self.labelOne.frame.origin.y = 80
+            self.labelOne.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        }) { completed in
+            
+        }
+        
+        UIView.animate(withDuration: 2.5, delay: 1, options: [.curveEaseOut], animations: {
+            self.labelTwo.alpha = 1
+            self.labelTwo.transform = CGAffineTransform(scaleX: 1.8, y: 1.8)
+        }) { completed in
+            
         }
     }
     
-    
-    
     func setUpLayout() {
-        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        labelOne.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        labelOne.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        
+        labelTwo.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        labelTwo.topAnchor.constraint(equalTo: labelOne.bottomAnchor, constant: 50).isActive = true
     }
 }
 
