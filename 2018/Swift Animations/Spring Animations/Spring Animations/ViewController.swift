5dc5193e75536df15bfbf6a24c99b5b619eefd29
Caleb Wells <calebrwells@gmail.com>
2018-04-24 16:36:18 -0700

    Added new text fields.

--- 2018/Swift Animations/Spring Animations/Spring Animations/ViewController.swift
+++ 2018/Swift Animations/Spring Animations/Spring Animations/ViewController.swift
@@ -23,11 +23,30 @@ class ViewController: UIViewController {
         let segmentedControl = UISegmentedControl(items: ["Sign In", "Register"])
         segmentedControl.selectedSegmentIndex = 0
         segmentedControl.tintColor = .white
+        segmentedControl.addTarget(self, action: #selector(handleSegmentedControl(_:)), for: .valueChanged)
         return segmentedControl
     }()
     
     let textField: UITextField = {
         let textField = UITextField()
+        textField.placeholder = "Name"
+        textField.backgroundColor = .white
+        textField.borderStyle = UITextBorderStyle.roundedRect
+        return textField
+    }()
+    
+    let textFieldTwo: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.backgroundColor = .white
+        textField.borderStyle = UITextBorderStyle.roundedRect
+        return textField
+    }()
+    
+    let textFieldThree: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Email"
+        textField.backgroundColor = .white
         textField.borderStyle = UITextBorderStyle.roundedRect
         return textField
     }()
@@ -35,6 +54,12 @@ class ViewController: UIViewController {
     let button: UIButton = {
         let button = UIButton()
         button.setTitle("Continue", for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 12
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
         button.addTarget(self, action: #selector(handleButtonPress), for: .touchUpInside)
         return button
     }()
@@ -44,7 +69,7 @@ class ViewController: UIViewController {
         label.text = "Thanks for signing up!"
         label.textColor = .white
         label.textAlignment = .center
-        label.font = UIFont.boldSystemFont(ofSize: 27)
+        label.font = UIFont.boldSystemFont(ofSize: 12)
         return label
     }()
     
@@ -58,35 +83,64 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         label.alpha = 0
+        button.alpha = 0
+        textFieldThree.isHidden = true
     }
     
     override func viewDidAppear(_ animated: Bool) {
         animateInWithSpring()
+        showButton()
     }
     
-    // MARK: === Handle actions
+    // MARK: === Handle actions.
+    @objc func handleSegmentedControl(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 1 {
+            showTextField()
+        } else {
+            hideTextField()
+        }
+    }
+    
     @objc func handleButtonPress() {
-        
+        print("=== tap?")
     }
     
+    // MARK: === Animations and transitions.
     func animateInWithSpring() {
         UIView.animate(withDuration: 2.0, delay: 0.25, usingSpringWithDamping: 0.4, initialSpringVelocity: 0, options: [], animations: {
-            
             self.label.alpha = 1
             self.label.frame.origin.y -= 50
-            
-            
         }, completion: nil)
         
     }
     
+    func showButton() {
+        UIView.transition(with: button, duration: 2.0, options: [.transitionCurlDown], animations: {
+            self.button.alpha = 1
+        }, completion: nil)
+    }
+    
+    func showTextField() {
+        UIView.transition(with: textFieldThree, duration: 0.5, options: [.transitionFlipFromTop], animations: {
+            self.textFieldThree.isHidden = false
+        }, completion: nil)
+    }
+    
+    func hideTextField() {
+        UIView.transition(with: textFieldThree, duration: 0.5, options: [.transitionFlipFromBottom], animations: {
+            self.textFieldThree.isHidden = true
+        }, completion: nil)
+    }
+    
+    // MARK: === Set up the stack view.
     func setUpStackView() {
-        let stackView = UIStackView(arrangedSubviews: [label,segmentedControl,textField,button,footerLabel])
+        let stackView = UIStackView(arrangedSubviews: [label,segmentedControl,textField,textFieldTwo,textFieldThree,button,footerLabel])
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 24
-        stackView.setCustomSpacing(100, after: label)
-        stackView.setCustomSpacing(180, after: button)
+        
+        stackView.setCustomSpacing(70, after: label)
+        stackView.setCustomSpacing(210, after: button)
         
         view.addSubview(stackView)
         
@@ -95,7 +149,7 @@ class ViewController: UIViewController {
             stackView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
             stackView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -72),
             stackView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, constant: -50)
-            ])
+        ])
     }
 
 }
