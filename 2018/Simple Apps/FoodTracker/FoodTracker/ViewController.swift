b1763cf937a3c36445fa2e1644fa90c0b6358a42
Caleb Wells <calebrwells@gmail.com>
2018-04-24 21:05:59 -0700

    Basic UI with a UIStackView

--- 2018/Simple Apps/FoodTracker/FoodTracker/ViewController.swift
+++ 2018/Simple Apps/FoodTracker/FoodTracker/ViewController.swift
@@ -8,12 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController {
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "Meal Name"
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        return label
+    }()
+    
+    let textField: UITextField = {
+        let text = UITextField()
+        text.placeholder = "Enter meal name"
+        text.borderStyle = UITextBorderStyle.roundedRect
+        return text
+    }()
+    
+    let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Set default label text", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        setUpStackView()
+    }
+    
+    func setUpStackView() {
+        let stackView = UIStackView(arrangedSubviews: [label, textField, button])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.spacing = 12
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            stackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            stackView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, constant: -100)
+        ])
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.widthAnchor.constraint(equalTo: stackView.widthAnchor).isActive = true
     }
 }
 
