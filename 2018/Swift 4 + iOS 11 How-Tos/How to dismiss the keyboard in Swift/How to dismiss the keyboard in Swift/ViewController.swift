6d54b83141489e93afa8fef0ac7b81389fb73f61
Caleb Wells <calebrwells@gmail.com>
2018-05-03 19:51:01 -0700

    Made a short video about textFieldDidEndEditing.

--- 2018/Swift 4 + iOS 11 How-Tos/How to dismiss the keyboard in Swift/How to dismiss the keyboard in Swift/ViewController.swift
+++ 2018/Swift 4 + iOS 11 How-Tos/How to dismiss the keyboard in Swift/How to dismiss the keyboard in Swift/ViewController.swift
@@ -17,18 +17,27 @@ class ViewController: UIViewController, UITextFieldDelegate {
         text.borderStyle = UITextBorderStyle.roundedRect
         text.autocorrectionType = UITextAutocorrectionType.yes
         text.keyboardType = UIKeyboardType.default
-        text.returnKeyType = UIReturnKeyType.default
+        text.returnKeyType = UIReturnKeyType.done
         text.clearButtonMode = UITextFieldViewMode.whileEditing
         text.contentVerticalAlignment = UIControlContentVerticalAlignment.center
         text.translatesAutoresizingMaskIntoConstraints = false
         return text
     }()
+    
+    
+    let label = UILabel()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addSubview(textField)
         textField.delegate = self
+        
+        view.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello, label"
         
         setUpLayout()
     }
@@ -38,9 +47,18 @@ class ViewController: UIViewController, UITextFieldDelegate {
         return false
     }
     
+    // The textFieldDidEndEditing(_:) method gives you a chance to read the information entered into the text field and do something with it.
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        label.text = textField.text
+    }
+    
+    
     func setUpLayout() {
         textField.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
         textField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        label.topAnchor.constraint(equalTo: textField.topAnchor, constant: -40).isActive = true
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
 }
 
