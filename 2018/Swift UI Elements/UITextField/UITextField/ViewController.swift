a8dd0718fb8a35f16ddd0ce5846d740b4f4f6ed6
Caleb Wells <calebrwells@gmail.com>
2018-05-24 09:36:58 -0700

    UITextField

---
+++ 2018/Swift UI Elements/UITextField/UITextField/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  UITextField
+//
+//  Created by Caleb Wells on 5/24/18.
+//  Copyright © 2018 Caleb Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
