244cd589d834574571460cdc34c2196185c2c6e3
Caleb Wells <calebrwells@gmail.com>
2018-05-26 15:18:05 -0700

    new video.

---
+++ 2018/Today Widget/Today Widget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Today Widget
+//
+//  Created by Caleb Wells on 5/26/18.
+//  Copyright © 2018 Caleb Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
