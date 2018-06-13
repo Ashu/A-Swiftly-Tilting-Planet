c62c963631536e66a35d50a8351c213158a41c50
Caleb Wells <calebrwells@gmail.com>
2018-06-12 22:33:45 -0600

    added editButtonItem, and UserDefaults.

--- 2018/Simple Apps/Notes/Notes/ViewController.swift
+++ 2018/Simple Apps/Notes/Notes/ViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class ViewController: UITableViewController {
     
     let cellId = "cellId"
+    let notes = "notes"
     
     var data = [String]()
 
@@ -14,6 +15,9 @@ class ViewController: UITableViewController {
         
         let addRowButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addRow))
         navigationItem.rightBarButtonItem = addRowButton
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        load()
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
@@ -32,11 +36,33 @@ class ViewController: UITableViewController {
         return cell!
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            data.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .right)
+        }
+        
+        save()
+    }
+    
     @objc func addRow() {
         let name = "Item \(data.count + 1)"
         data.insert(name, at: 0)
         
         let indexPath = IndexPath(row: 0, section: 0)
         tableView.insertRows(at: [indexPath], with: .left)
+        
+        save()
+    }
+    
+    func save() {
+        UserDefaults.standard.set(data, forKey: notes)
+    }
+    
+    func load() {
+        if let loadedData: [String] = UserDefaults.standard.value(forKey: notes) as? [String] {
+            data = loadedData
+            tableView.reloadData()
+        }
     }
 }
