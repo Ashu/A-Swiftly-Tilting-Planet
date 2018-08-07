9b7bfc962cf080f1f791d7c9c605e260f77ffb0b
Caleb Wells <calebrwells@gmail.com>
2018-08-07 16:52:33 -0700

    Starting to play around with ARKit again.

---
+++ 2018/ARKit 2/ARKit 2/ARKit 2/Scene.swift
@@ -0,0 +1,105 @@
+import SpriteKit
+import ARKit
+
+var score = 0
+
+enum GameState {
+    case home
+    case playing
+}
+
+class Scene: SKScene {
+    
+    var playButton: SKSpriteNode!
+    var gcButton: SKSpriteNode!
+    
+    var gameState = GameState.home
+    
+    var anchorCount = 0
+    
+    override func didMove(to view: SKView) {
+        createMenu()
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if gameState == .playing {
+            anchorCount += 1
+            
+            if gameState == .playing && anchorCount == 20 {
+                createAnchor()
+                anchorCount = 0
+            }
+        }
+    }
+    
+    //MARK: ==== Create the menus
+    func createMenu() {
+        playButton = SKSpriteNode(imageNamed: "play")
+        playButton.position = CGPoint(x: frame.midX + 200, y: frame.midY)
+        playButton.size = CGSize(width: 200, height: 100)
+        addChild(playButton)
+        
+        gcButton = SKSpriteNode(imageNamed: "gc")
+        gcButton.position = CGPoint(x: frame.midX - 200, y: frame.midY)
+        gcButton.size = CGSize(width: 200, height: 100)
+        addChild(gcButton)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let point = touch.location(in: self.view)
+        
+        switch gameState {
+        case .home:
+            if point.x > (view?.frame.width)! / 2 {
+                gameState = .playing
+                let fadeOut = SKAction.fadeOut(withDuration: 0.5)
+                let remove = SKAction.removeFromParent()
+                let wait = SKAction.wait(forDuration: 0.5)
+                let sequence = SKAction.sequence([fadeOut, wait, remove])
+                playButton.run(sequence)
+                gcButton.run(sequence)
+            }
+        case .playing:
+            goHome()
+        }
+    }
+    
+    func goHome() {
+        let gameOverLabel = SKLabelNode(text: "Game Over")
+        addChild(gameOverLabel)
+        
+        let fadeIn = SKAction.fadeIn(withDuration: 1.5)
+        let remove = SKAction.removeFromParent()
+        let wait = SKAction.wait(forDuration: 1)
+        let gameOverSequence = SKAction.sequence([fadeIn, wait, remove])
+        gameOverLabel.run(gameOverSequence)
+        
+        let transitioWait = SKAction.wait(forDuration: 1)
+        let action = SKAction.run {
+            let scene = Scene(fileNamed: "Scene")!
+            let transition = SKTransition.moveIn(with: .up, duration: 0.1)
+            self.view?.presentScene(scene, transition: transition)
+        }
+        self.run(SKAction.sequence([transitioWait, action]))
+    }
+    
+    func createAnchor() {
+        guard let sceneView = self.view as? ARSKView else {
+            return
+        }
+        
+        // Create anchor using the camera's current position
+        if let currentFrame = sceneView.session.currentFrame {
+            
+            // Create a transform with a translation of 0.2 meters in front of the camera
+            var translation = matrix_identity_float4x4
+            translation.columns.3.z = -0.4
+            let transform = simd_mul(currentFrame.camera.transform, translation)
+            
+            // Add a new anchor to the session
+            let anchor = ARAnchor(transform: transform)
+            sceneView.session.add(anchor: anchor)
+        }
+    }
+}
---
+++ 2018/ARKit 2/ARKit 2/ARKit 2/ViewController.swift
@@ -0,0 +1,137 @@
+import UIKit
+import SpriteKit
+import ARKit
+import GameKit
+
+let leaderboardID = "bestDuckyHuntARScore"
+
+class ViewController: UIViewController, ARSKViewDelegate, GKGameCenterControllerDelegate {
+    
+    @IBOutlet var sceneView: ARSKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        authenticateWithGameCenter()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and node count
+        sceneView.showsFPS = true
+        sceneView.showsNodeCount = true
+        
+        // Load the SKScene from 'Scene.sks'
+        if let scene = SKScene(fileNamed: "Scene") {
+            sceneView.presentScene(scene)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    // MARK: - ARSKViewDelegate
+    
+    func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
+        // Create and configure a node for the anchor added to the view's session.
+        let labelNode = SKLabelNode(text: "👾")
+        labelNode.horizontalAlignmentMode = .center
+        labelNode.verticalAlignmentMode = .center
+        return labelNode;
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+    
+    // MARK: === Game Center code
+    
+    var gkEnabled = Bool()
+    var gkDefaultLeaderBoardID = String()
+    
+    func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
+        gameCenterViewController.dismiss(animated: true, completion: nil)
+    }
+    
+    func authenticateWithGameCenter() {
+        let localPlayer: GKLocalPlayer = GKLocalPlayer.local
+        
+        localPlayer.authenticateHandler = {(ViewController, error) -> Void in
+            if (ViewController) != nil {
+                self.present(ViewController!, animated: true, completion: nil)
+            } else if localPlayer.isAuthenticated {
+                self.gkEnabled = true
+                
+                localPlayer.loadDefaultLeaderboardIdentifier(completionHandler: {(leaderboardID, error) in
+                    if error != nil {
+                        print(error!)
+                    } else {
+                        self.gkDefaultLeaderBoardID = leaderboardID!
+                    }
+                })
+            } else {
+                self.gkEnabled = false
+                print(error!)
+            }
+        }
+    }
+    
+    func submitScoreToGC() {
+        // submit score to Game Center
+        let bestScore = GKScore(leaderboardIdentifier: leaderboardID)
+        bestScore.value = Int64(score)
+        GKScore.report([bestScore]) { (error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            } else {
+                // code?
+            }
+        }
+    }
+    
+    func openGameCenter() {
+        let gameCenterVC = GKGameCenterViewController()
+        gameCenterVC.gameCenterDelegate = self
+        gameCenterVC.viewState = .leaderboards
+        gameCenterVC.leaderboardIdentifier = leaderboardID
+        present(gameCenterVC, animated: true, completion: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let point = touch.location(in: self.view)
+        
+        if point.x < (view?.frame.width)! / 2 {
+            openGameCenter()
+        }
+    }
+}
