1742223e9fedd79d25a65e1d8c21a9cfc851a2db
Caleb Wells <calebrwells@gmail.com>
2018-09-10 12:02:54 -0700

    ...

---
+++ 2018/iOS Networking with Swift/Create a URL.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+let url = URL(string: "https://apple.com")!
+
+print(url.scheme!)
+print(url.host!)
+print(url.absoluteURL)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2018/iOS Networking with Swift/USDA Food Composition Databases.playground/Contents.swift
@@ -0,0 +1,3 @@
+import UIKit
+
+var url = "https://api.nal.usda.gov/ndb/V2/reports?ndbno=01009&ndbno=45202763&ndbno=35193&type=f&format=json&api_key=DEMO_KEY"
