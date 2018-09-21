2cc5e526cf797c2ce8f9f4f9e4914baa183f97a3
Caleb Wells <calebrwells@gmail.com>
2018-09-21 13:06:36 -0700

    New video in Xcode 10 tutorial for beginners playlist

---
+++ 2018/Xcode 10 tutorial for beginners/Project 1/Project 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 1
+//
+//  Created by Caleb Wells on 9/21/18.
+//  Copyright © 2018 Caleb Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
