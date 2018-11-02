faf4e7d4b6f3c2a51622c4263b648ae88e0af24a
Caleb Wells <calebrwells@gmail.com>
2018-11-02 10:35:20 -0600

    new video.

---
+++ 2018/Xcode 10 tutorial for beginners/Live View Playground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+import PlaygroundSupport
+
+class TableViewControllor: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+PlaygroundPage.current.liveView = TableViewControllor()
