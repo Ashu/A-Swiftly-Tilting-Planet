cd61d69bc89f90f3bb8cdb728f60cf01189806ea
Caleb Wells <calebrwells@gmail.com>
2018-11-12 22:48:15 -0700

    New video.

---
+++ 2018/Xcode 10 tutorial for beginners/MyPlayground.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+let imageView = UIImageView()
+imageView.contentMode = .scaleAspectFit
+imageView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+imageView.image = UIImage(imageLiteralResourceName: "sunset.jpg")
