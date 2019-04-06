60837fbe1936f604c0c1861dc99e11cd6cd1133a
Caleb Wells <calebrwells@gmail.com>
2019-04-05 20:58:42 -0600

    Added the AppDelegate

---
+++ 2019/Friday Night Swift/Night Three/Night Three/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Night Three
+//
+//  Created by Caleb Wells on 4/5/19.
+//  Copyright © 2019 Caleb Wells. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
