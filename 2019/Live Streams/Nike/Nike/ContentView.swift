adfbf67d025af2379a6e7e9983d526dac35aa617
Caleb Wells <calebrwells@gmail.com>
2019-10-02 00:19:58 -0600

    New stream.

---
+++ 2019/Live Streams/Nike/Nike/ContentView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            NavigationView { FeedView() }
+            .tabItem {
+                Image(systemName: "house.fill")
+            }.tag(0)
+            
+            NavigationView { Text("View 2 coming soon?") }
+            .tabItem {
+                Image(systemName: "hare.fill")
+            }.tag(1)
+            
+            NavigationView { Text("View 3 coming soon?") }
+            .tabItem {
+                Image(systemName: "bag.fill")
+            }.tag(2)
+            
+            NavigationView { Text("View 4 coming soon?") }
+            .tabItem {
+                Image(systemName: "tray.full.fill")
+            }.tag(3)
+            
+            NavigationView { Text("View 5 coming soon?") }
+            .tabItem {
+                Image(systemName: "person.fill")
+            }.tag(4)
+        }.edgesIgnoringSafeArea([.top, .horizontal])
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 2019/Live Streams/Nike/Nike/FeedView.swift
@@ -0,0 +1,65 @@
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        List {
+            ZStack {
+                Image("jogging")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: UIScreen.main.bounds.width, height: 600, alignment: .leading)
+                    .padding(.leading, -16)
+                
+                Rectangle()
+                    .frame(width: UIScreen.main.bounds.width, height: 600, alignment: .leading)
+                    .padding(.leading, -16)
+                    .foregroundColor(.black)
+                    .opacity(0.2)
+                
+                VStack {
+                    Text("MEMBER ACCESS")
+                        .foregroundColor(.white)
+                        .fontWeight(.heavy)
+                    
+                    Text("REACT")
+                        .font(.system(size: 72))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.bottom, -38)
+                    
+                    Text("BY YOU")
+                        .font(.system(size: 72))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.bottom, -18)
+                        
+                    
+                    Text("ONE CREW")
+                        .foregroundColor(.white)
+                        .fontWeight(.heavy)
+                        .padding(.bottom, 100)
+                                        
+                    HStack {
+                        Button(action: {
+                            print("Button!")
+                        }) {
+                            Text("LEARN MORE")
+                                .foregroundColor(.white)
+                                .padding()
+                                .border(Color.white, width: 1)
+                        }
+                        
+                        Spacer()
+                    }
+                }
+            }
+        }
+        .navigationBarTitle("FEED", displayMode: .inline)
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
